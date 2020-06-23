d9827a035835d08a3e0955e3ebd311f61dd56ae6
13301416380 <88197704@qq.com>
2020-06-23 21:39:22 +0800

    helloword

---
+++ src/main/java/com/njweb/resources/HelloWord.java
@@ -0,0 +1,13 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2020\6\23 0023
+ * Time: 21:32
+ */
+package com.njweb.resources;
+
+public class HelloWord {
+    public String helloWord(){
+        return "你好";
+    }
+}
